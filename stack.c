02326fcff852b3b73259f46cb3b90bd84c2f1cb7
ROHAN PH <rohankumarph5135@gmail.com>
2025-12-01 23:56:45 +0530

    pop fucnction

--- stack.c
+++ stack.c
@@ -1,7 +1,3 @@
-// stack is linear data sructure which follows lifo principle where every new element at one end called top and removed from there
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #define max 3
@@ -11,30 +7,58 @@ int isFull(){
     return(top==max-1);
 }
 void push(int data){
-    if(isFull()){
-        printf("\nstack overfull");
-        return;
-    }
+   
     top++;
     arr[top]=data;
 
 }
+int isEmpty(){
+    return(top==-1);
+}
+int pop(){
+      if(isEmpty())
+        {
+            printf("the stack is empty\n");
+            return 0;
+         }
+    int data=arr[top];
+    top--;
+    return data;
+}
 int main(){
     int n,c;
-    printf("1:push  2:exit");
-    while(1){
+    printf("1:push  2:exit 3:pop ");
+    while(1)
+    {
         printf("choice>>");
         scanf("%d",&c);
-        if(c==2){
+        switch(c){
+            
+        case 1:
+              {
+                 if(isFull()){
+                 printf("stack overfull\n");
+                 break;
+                 }
+                 else
+                 {
+                 printf("eneter the eelement>>");
+                 scanf("%d",&n);
+                 push(n);
+                 }
         break;
-    }
-    printf("eneter the eelement");
-    scanf("%d",&n);
-    push(n);
-    }
-    
+           }
+        case 2:{
+            exit(0);   
+             }
+        case 3:{
+          printf("%d\n",pop());  
+          break;
+        }
+    }}
+    printf("the stack elements are>>");
 for(int i=top;i>=0;i--){
-    printf("%d",arr[i]);
+    printf("%d\t",arr[i]);
 }
     
 }
